f2db7d0dfb3fca4d492aa21c71fe4d7034b43d48
Aniket <asp310768@gmail.com>
2024-07-10 19:22:21 +0530

    first commit

--- pattern34.cpp
+++ pattern34.cpp
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 /*
-Hallow Pyramid
+Hallow Pyramid 
+
     *
    * *
   *   *
@@ -64,39 +64,3 @@ int main(){
 //         cout<<endl;
 //     }
 // }
-=======
-/*
-Hallow Pyramid
-    *
-   * *
-  *   *
- *     *
-*********
-
-*/
-
-#include<iostream>
-using namespace std;
-
-int main(){
-    int n;
-    cout<<"Enter no. of rows : ";
-    cin>>n;
-
-    for(int i=1;i<=n;i++){
-        for(int space = n-i;space;space--){
-            cout<<" ";
-        }
-
-        for(int j=1;j<=2*i-1;j++){
-            if(j==1 || j==2*i-1 || i==n){
-                cout<<"*";
-            }
-            else{
-                cout<<" ";
-            }
-        }
-        cout<<endl;
-    }
-}
->>>>>>> 2f50ac0cc0c4dc6d57cfc13161e0dc5a3dd55df0
